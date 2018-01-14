2865aa34713e61c6191f755ea5bb41e3312e0873
xschen <xs0040@gmail.com>
2018-01-14 12:21:57 +0800

    refactor the convolutional and recurrent anomaly detector

--- keras_anomaly_detection/library/convolutional.py
+++ keras_anomaly_detection/library/convolutional.py
@@ -11,10 +11,11 @@ class Conv1DAutoEncoder(object):
     def __init__(self):
         self.model = None
         self.time_window_size = None
+        self.metric = None
         self.config = None
 
     @staticmethod
-    def create_model(time_window_size):
+    def create_model(time_window_size, metric):
         model = Sequential()
         model.add(Conv1D(filters=256, kernel_size=5, padding='same', activation='relu',
                          input_shape=(time_window_size, 1)))
@@ -22,7 +23,7 @@ class Conv1DAutoEncoder(object):
 
         model.add(Dense(units=time_window_size, activation='linear'))
 
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_absolute_error'])
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=[metric])
         print(model.summary())
         return model
 
@@ -38,28 +39,32 @@ class Conv1DAutoEncoder(object):
     def get_architecture_file(model_dir_path):
         return model_dir_path + '/' + Conv1DAutoEncoder.model_name + '-architecture.json'
 
-    def fit(self, timeseries_dataset, model_dir_path, batch_size=None, epochs=None, validation_split=None):
+    def fit(self, timeseries_dataset, model_dir_path, batch_size=None, epochs=None, validation_split=None, metric=None):
         if batch_size is None:
             batch_size = 8
         if epochs is None:
             epochs = 20
         if validation_split is None:
             validation_split = 0.2
+        if metric is None:
+            metric = 'mean_absolute_error'
 
         self.time_window_size = timeseries_dataset.shape[1]
+        self.metric = metric
 
         input_timeseries_dataset = np.expand_dims(timeseries_dataset, axis=2)
         print(input_timeseries_dataset.shape)
 
         self.config = dict()
         self.config['time_window_size'] = self.time_window_size
+        self.config['metric'] = self.metric
         config_file_path = Conv1DAutoEncoder.get_config_file(model_dir_path=model_dir_path)
         np.save(config_file_path, self.config)
 
         weight_file_path = Conv1DAutoEncoder.get_weight_file(model_dir_path=model_dir_path)
         architecture_file_path = Conv1DAutoEncoder.get_architecture_file(model_dir_path)
         checkpoint = ModelCheckpoint(weight_file_path)
-        self.model = Conv1DAutoEncoder.create_model(self.time_window_size)
+        self.model = Conv1DAutoEncoder.create_model(self.time_window_size, metric=self.metric)
         open(architecture_file_path, 'w').write(self.model.to_json())
         self.model.fit(x=input_timeseries_dataset, y=timeseries_dataset,
                        batch_size=batch_size, epochs=epochs,
--- keras_anomaly_detection/library/recurrent.py
+++ keras_anomaly_detection/library/recurrent.py
@@ -12,15 +12,16 @@ class LstmAutoEncoder(object):
         self.model = None
         self.time_window_size = None
         self.config = None
+        self.metric = None
 
     @staticmethod
-    def create_model(time_window_size):
+    def create_model(time_window_size, metric):
         model = Sequential()
         model.add(LSTM(units=128, input_shape=(time_window_size, 1), return_sequences=False))
 
         model.add(Dense(units=time_window_size, activation='linear'))
 
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_absolute_error'])
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=[metric])
         print(model.summary())
         return model
 
@@ -36,27 +37,34 @@ class LstmAutoEncoder(object):
     def get_architecture_file(model_dir_path):
         return model_dir_path + '/' + LstmAutoEncoder.model_name + '-architecture.json'
 
-    def fit(self, timeseries_dataset, model_dir_path, batch_size=None, epochs=None, validation_split=None):
+    def fit(self, timeseries_dataset, model_dir_path, batch_size=None, epochs=None, validation_split=None, metric=None):
         if batch_size is None:
             batch_size = 8
         if epochs is None:
             epochs = 20
         if validation_split is None:
             validation_split = 0.2
+        if metric is None:
+            metric = 'mean_absolute_error'
 
+        self.metric = metric
         self.time_window_size = timeseries_dataset.shape[1]
+
+        input_timeseries_dataset = np.expand_dims(timeseries_dataset, axis=2)
+        print(input_timeseries_dataset.shape)
 
         self.config = dict()
         self.config['time_window_size'] = self.time_window_size
+        self.config['metric'] = self.metric
         config_file_path = LstmAutoEncoder.get_config_file(model_dir_path=model_dir_path)
         np.save(config_file_path, self.config)
 
         weight_file_path = LstmAutoEncoder.get_weight_file(model_dir_path=model_dir_path)
         architecture_file_path = LstmAutoEncoder.get_architecture_file(model_dir_path)
         checkpoint = ModelCheckpoint(weight_file_path)
-        self.model = LstmAutoEncoder.create_model(self.time_window_size)
+        self.model = LstmAutoEncoder.create_model(self.time_window_size, metric=self.metric)
         open(architecture_file_path, 'w').write(self.model.to_json())
-        self.model.fit(x=timeseries_dataset, y=timeseries_dataset,
+        self.model.fit(x=input_timeseries_dataset, y=timeseries_dataset,
                        batch_size=batch_size, epochs=epochs,
                        verbose=LstmAutoEncoder.VERBOSE, validation_split=validation_split,
                        callbacks=[checkpoint])
