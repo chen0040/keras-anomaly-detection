f69983292a71590150174ddb916f6de3936b9c7c
Kevin Hodges <khodges42@gmail.com>
2018-03-18 15:28:01 -0400

    Small tweaks
    Worked with this library and ended up making a few small tweaks, mostly just relating to scoping of self and some code cleanup.

--- keras_anomaly_detection/library/convolutional.py
+++ keras_anomaly_detection/library/convolutional.py
@@ -1,3 +1,8 @@
+'''
+Based on work by Xianshun Chen
+https://github.com/chen0040/keras-anomaly-detection
+'''
+
 from keras.layers import Conv1D, GlobalMaxPool1D, Dense, Flatten
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
@@ -41,41 +46,31 @@ class Conv1DAutoEncoder(object):
         return model_dir_path + '/' + Conv1DAutoEncoder.model_name + '-architecture.json'
 
     def load_model(self, model_dir_path):
-        config_file_path = Conv1DAutoEncoder.get_config_file(model_dir_path)
+        config_file_path = self.get_config_file(model_dir_path)
         self.config = np.load(config_file_path).item()
         self.metric = self.config['metric']
         self.time_window_size = self.config['time_window_size']
         self.threshold = self.config['threshold']
-        self.model = Conv1DAutoEncoder.create_model(self.time_window_size, self.metric)
-        weight_file_path = Conv1DAutoEncoder.get_weight_file(model_dir_path)
+        self.model = self.create_model(self.time_window_size, self.metric)
+        weight_file_path = self.get_weight_file(model_dir_path)
         self.model.load_weights(weight_file_path)
 
-    def fit(self, dataset, model_dir_path, batch_size=None, epochs=None, validation_split=None, metric=None,
-            estimated_negative_sample_ratio=None):
-        if batch_size is None:
-            batch_size = 8
-        if epochs is None:
-            epochs = 100
-        if validation_split is None:
-            validation_split = 0.2
-        if metric is None:
-            metric = 'mean_absolute_error'
-        if estimated_negative_sample_ratio is None:
-            estimated_negative_sample_ratio = 0.9
+    def fit(self, dataset, model_dir_path, batch_size=8, epochs=100, validation_split=0.1, metric='mean_absolute_error',
+            estimated_negative_sample_ratio=0.9):
 
         self.time_window_size = dataset.shape[1]
         self.metric = metric
 
         input_timeseries_dataset = np.expand_dims(dataset, axis=2)
 
-        weight_file_path = Conv1DAutoEncoder.get_weight_file(model_dir_path=model_dir_path)
-        architecture_file_path = Conv1DAutoEncoder.get_architecture_file(model_dir_path)
+        weight_file_path = self.get_weight_file(model_dir_path=model_dir_path)
+        architecture_file_path = self.get_architecture_file(model_dir_path)
         checkpoint = ModelCheckpoint(weight_file_path)
-        self.model = Conv1DAutoEncoder.create_model(self.time_window_size, metric=self.metric)
+        self.model = self.create_model(self.time_window_size, metric=self.metric)
         open(architecture_file_path, 'w').write(self.model.to_json())
         history = self.model.fit(x=input_timeseries_dataset, y=dataset,
                                  batch_size=batch_size, epochs=epochs,
-                                 verbose=Conv1DAutoEncoder.VERBOSE, validation_split=validation_split,
+                                 verbose=self.VERBOSE, validation_split=validation_split,
                                  callbacks=[checkpoint]).history
         self.model.save_weights(weight_file_path)
 
@@ -90,7 +85,7 @@ class Conv1DAutoEncoder(object):
         self.config['time_window_size'] = self.time_window_size
         self.config['metric'] = self.metric
         self.config['threshold'] = self.threshold
-        config_file_path = Conv1DAutoEncoder.get_config_file(model_dir_path=model_dir_path)
+        config_file_path = self.get_config_file(model_dir_path=model_dir_path)
         np.save(config_file_path, self.config)
 
         return history
